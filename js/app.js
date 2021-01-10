7c985eda3837ac307dbf689001d55c77838438f2
Ahmad Hassan <hassanahmadp@gmail.com>
2021-01-10 22:54:39 +0500

    midway

---
+++ js/app.js
@@ -0,0 +1,35 @@
+var slider = document.querySelector(".slider");
+var prev = document.querySelector(".prev");
+var next = document.querySelector(".next");
+
+prev.addEventListener("click", Slide);
+next.addEventListener("click", Slide);
+var counter = 0;
+
+checkBtn();
+
+function Slide(e) {
+    if (e.target.classList.contains("next")) {
+        counter++;
+        slider.style.transform = `translateX(-${counter * 390}px)`;
+    } else {
+        counter--;
+        slider.style.transform = `translateX(-${counter * 390}px)`;
+    }
+
+    checkBtn();
+}
+
+function checkBtn() {
+    if (counter < 8) {
+        next.style.display = "block";
+    } else {
+        next.style.display = "none";
+    }
+    
+    if (counter > 0) {
+        prev.style.display = "block";
+    } else {
+        prev.style.display = "none";
+    }
+}
